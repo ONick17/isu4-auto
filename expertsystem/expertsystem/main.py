b0e6ed98edab443aa6cd0bd9ff316b983b7a36b1
Vlad Shadrin <shadrin.vlad.2001@gmail.com>
2023-11-23 00:34:09 +0800

    Interface v0.1

--- expertsystem/expertsystem/main.py
+++ expertsystem/expertsystem/main.py
@@ -5,7 +5,23 @@
 4. Игра => Предложение заготовленных скриптов, прогон игры с учётом скрипта и графиков, построение графиков
 '''
 
-#этап 0: pandas для работы с csv
+#этап 0
+#pandas для работы с csv
+#PyQt6  для интерфейса
+#sys для открытия проводника
+#qt_main - файл с интерфейсом главного окна
 import pandas as pd
+from PyQt6 import QtWidgets
+from qt_main import MyMainWindow
+import sys
 
 #этап 1: считывание csv, накидывание погрешности (15%)
+
+#Код, запускающий интерфейс
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    main_window = MyMainWindow()
+    main_window.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec())
