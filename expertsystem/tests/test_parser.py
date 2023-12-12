ba5d99662e465c083c10e6c4d7f804a8068aa7c8
L140-beep <kartoshka0302@mail.ru>
2023-12-12 17:21:56 +0800

    Дофига всего

--- expertsystem/tests/test_parser.py
+++ expertsystem/tests/test_parser.py
@@ -1,11 +1,42 @@
-import pytest
-import os
+import pandas as pd
 
 try:
-    from expertsystem.parser import parse
+    from expertsystem.expertsystem.myparser import DataProcessor, MostValuedConsumer, MostValuedProducer
 except ImportError:
-    from .parser import parse
+    from expertsystem.myparser import DataProcessor, MostValuedConsumer, MostValuedProducer
 
 
 def test_parse():
-    parse('/home/l140/education/data/expertsystem/forecast.csv')
+    proc = DataProcessor()
+    proc.parse('./expertsystem/forecast.csv')
+
+
+def test_getMostValued():
+    data = pd.DataFrame({
+        'Солнце': [5.5, 5.5],
+        'Ветер': [3.4, 2.3],
+        'Дома': [4.0, 5.0],
+        'Заводы': [2.0, 5.0],
+        'Больницы': [3.0, 1.0]
+    })
+    most_valued_consumer = MostValuedConsumer('Дома', 9.0)
+    most_valued_producer = MostValuedProducer('Солнце', 11.0
+                                              )
+    proc = DataProcessor()
+    proc.prepare_data(data)
+    assert proc.getMostValued() == (
+        most_valued_consumer, most_valued_producer)
+
+
+def test_base_value_0():
+    data = pd.DataFrame({
+        'Солнце': [5.5, 5.5],
+        'Ветер': [3.4, 2.3],
+        'Дома': [4.0, 5.0],
+        'Заводы': [2.0, 5.0],
+        'Больницы': [3.0, 1.0]
+    })
+
+    proc = DataProcessor()
+    proc.prepare_data(data)
+    proc.get_base_value_0()
